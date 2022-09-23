dde41c8596d3d8910c13b4c68c7e8962616a9fba
Alex <you@example.com>
2022-09-23 15:32:50 -0400

    some more testing

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # FLOPSbrowser
-A Fast, Lightweight, Open Source, Private, and Secure Browsing Experience
+A Fast, Lightweight, Open Source, Private, and Secure Browsing Experience <3
--- flops.cpp
+++ flops.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    cout << "Spaghetti O's!";
+    //this is just me testing out print statements
+    /* ooga
+        badooga */
+    cout << "Spaghetti O's!\n";
+    cout << "and oreos!" << endl;
+    cout << "poopy\tpeepee";
+
+    int x, y;
+    cout << "\n\nCalculator Time! Please enter a number: ";
+    cin >> x;
+    cout << "Enter a second number: ";
+    cin >> y;
+    cout << x + y;
+
+    string poopy = "AHHHH";
+    cout << poopy;
     return 0;
 }
